0d904543085abe06fd73d9864ccba7b36bf42e62
Ikaro C. Laborda <iclaborda@msn.com>
2023-06-24 14:35:02 -0300

    feat: implementing base structure for api controllers for post and topic

---
+++ app/Http/Controllers/Api/V1/TopicController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Topic;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Topic $topic
+     * @return Response
+     */
+    public function show(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Topic $topic
+     * @return Response
+     */
+    public function edit(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Topic $topic
+     * @return Response
+     */
+    public function update(Request $request, Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Topic $topic
+     * @return Response
+     */
+    public function destroy(Topic $topic)
+    {
+        //
+    }
+}
