19a1b73589b7c4536deffc4ce853a1db4967bcff
Ikaro C. Laborda <iclaborda@msn.com>
2023-06-24 14:33:47 -0300

    fix: modifying user model to relate to posts

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -37,4 +38,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+         public function posts(): HasMany
+        {
+            return $this->hasMany(Post::class);
+        }
 }
