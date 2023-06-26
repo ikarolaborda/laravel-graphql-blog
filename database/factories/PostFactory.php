faabf3e402a78ea46dc5efe038dc1a5884008ba4
Ikaro C. Laborda <iclaborda@msn.com>
2023-06-26 19:40:33 -0300

    feat: implementing logic for factories, seeders and migration order

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,12 +1,17 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Post;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence(5),
+        'content' => $faker->paragraph(10),
+        'lead' => $faker->paragraph(2),
+        'topic_id' => rand(1, 6),
+        'user_id' => rand(1, 20),
     ];
 });
--- database/seeds/TopicSeeder.php
+++ database/seeds/TopicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Topic;
 use Illuminate\Database\Seeder;
 
 class TopicSeeder extends Seeder
@@ -11,6 +12,11 @@ class TopicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Topic::create(['Name' => 'General', 'Slug' => 'general']);
+        Topic::create(['Name' => 'Help', 'Slug' => 'help']);
+        Topic::create(['Name' => 'Announcements', 'Slug' => 'announcements']);
+        Topic::create(['Name' => 'Tips and Tricks', 'Slug' => 'tips-and-tricks']);
+        Topic::create(['Name' => 'Tutorials', 'Slug' => 'tutorials']);
+        Topic::create(['Name' => 'Packages', 'Slug' => 'packages']);
     }
 }
